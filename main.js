798a2e4a35cd241d2c10f1cab7feda2a43339920
Swaelo <harleylaurie@rocketmail.com>
2018-09-22 23:17:20 +1000

    comments

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
+//Import discord command library
 const Commando = require('discord.js-commando');
+//Load config file containing login token etc
 var config = require('./package.json');
-const bot = new Commando.Client();
 
-//Add groups of commands for added bot functionality
+//Initialise and setup the bot client
+const bot = new Commando.Client();
+//Add groups of commands for added functionality
 bot.registry.registerGroup('jukebox', 'Jukebox');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands");
-
-//Globals
+//Maintain a list of servers in which we are connected to
+//This is used to keep the different music queues of
+//different servers seperated from one another
 global.servers = {};
 
-//when the bot connects to the server
+//Event Trigger: when connected to a new server
 bot.on('ready', function()
 {
     console.log("connected");
 });
 
-//whenever a message is sent
+//Event Trigger: when a new message is sent to a server we are connected to
 bot.on('message', function(message)
 {
     if(message.content == 'Hello')
@@ -26,4 +30,5 @@ bot.on('message', function(message)
     }
 });
 
+//Setup complete, now connect the bot to any servers it has been invited to
 bot.login(config.token);
